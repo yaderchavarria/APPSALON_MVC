2ee348f12871826bbe674cf7ad3af8d91547bbe8
yaderchavarria <yaderchavarria.r@gmail.com>
2022-10-31 13:28:13 -0600

    Solucionn problema base de datos

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD;CHARSET=utf8mb4']);
 
 
 if (!$db) {
